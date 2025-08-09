06608b8aa4aafc4233684eae9f58c005343953c0
adarshkscoorg123 <adarsh_coorg123@yahoo.com>
2025-08-09 15:22:23 +0530

    4th August

--- src/test/java/com/thetestingacademy/sample/RestAssuredHelloWorld.java
+++ src/test/java/com/thetestingacademy/sample/RestAssuredHelloWorld.java
@@ -1,4 +1,25 @@
 package com.thetestingacademy.sample;
 
+import io.restassured.RestAssured;
+
 public class RestAssuredHelloWorld {
+
+    public static void main(String[] args) {
+
+        //https://restful-booker.herokuapp.com/ping
+        // Response 201 is created or not
+
+        // Gherkin Syntax-Given,When,Then
+
+        RestAssured.given()
+                           .baseUri("https://restful-booker.herokuapp.com")
+                           .basePath("ping")
+
+                           .when()
+                            .log().all().get()
+
+                            .then()
+                            .log().all().statusCode(200);
+
+    }
 }
